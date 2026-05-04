92d706fcd8af6818492279e2be79565cdebc6b49
Steven Fitzpatrick <steven@fitz.gg>
2026-05-04 23:46:19 +0000

    Add umami analytics script

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
 	import { page } from '$app/state';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-
+	import {env} from '$env/dynamic/public';
 	let { children } = $props();
 
 	let isLanding = $derived(page.url.pathname === '/');
@@ -13,7 +14,15 @@
 <svelte:head>
 	<title>Symbology - Investment Analysis Platform</title>
 	<meta name="description" content="Explore LLM-generated insights on publicly traded companies." />
+        <link rel="icon" href={favicon} />
+        <script
+                defer
+                src={env.PUBLIC_UMAMI_SCRIPT_URL}
+                data-website-id={env.PUBLIC_UMAMI_WEBSITE_ID}
+    data-performance="true"
+        ></script>
 </svelte:head>
+
 
 <ModeWatcher />
 
